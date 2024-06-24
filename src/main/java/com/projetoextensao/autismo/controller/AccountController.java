9015330347f0813587334ea2efd59cc8c1d80aa9
Luis Gabriel <barros.luissalv@gmail.com>
2024-06-24 19:21:52 -0300

    Comentar registerAccount

--- src/main/java/com/projetoextensao/autismo/controller/AccountController.java
+++ src/main/java/com/projetoextensao/autismo/controller/AccountController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projetoextensao.autismo.dto.account.AccountFormDTO;
+//import com.projetoextensao.autismo.dto.account.AccountFormDTO;
 import com.projetoextensao.autismo.dto.account.AccountLoginDTO;
 import com.projetoextensao.autismo.dto.account.AccountResponseDTO;
-import com.projetoextensao.autismo.model.entities.enums.TypeAccount;
+//import com.projetoextensao.autismo.model.entities.enums.TypeAccount;
 import com.projetoextensao.autismo.service.AccountService;
 
-import jakarta.servlet.http.HttpSession;
+//import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RestController
@@ -25,21 +25,21 @@ public class AccountController {
 	@Autowired
 	private AccountService service;
 	
-	@PostMapping
-	public ResponseEntity<Void> registerAccount(@RequestBody AccountFormDTO accountDTO, HttpSession session) {
-		System.out.println(accountDTO);
-		
-		if (accountDTO.account().equals(TypeAccount.EMPLOYEE)) {
-			
-			session.setAttribute("employeeData", accountDTO);
-			return new ResponseEntity<>(HttpStatus.PERMANENT_REDIRECT);
-		} else if (accountDTO.account().equals(TypeAccount.EMPLOYER)) {
-			
-			session.setAttribute("employerData", accountDTO);
-			return new ResponseEntity<>(HttpStatus.PERMANENT_REDIRECT);
-		}
-		return null;
-	}
+//	@PostMapping
+//	public ResponseEntity<Void> registerAccount(@RequestBody AccountFormDTO accountDTO, HttpSession session) {
+//		System.out.println(accountDTO);
+//		
+//		if (accountDTO.account().equals(TypeAccount.EMPLOYEE)) {
+//			
+//			session.setAttribute("employeeData", accountDTO);
+//			return new ResponseEntity<>(HttpStatus.PERMANENT_REDIRECT);
+//		} else if (accountDTO.account().equals(TypeAccount.EMPLOYER)) {
+//			
+//			session.setAttribute("employerData", accountDTO);
+//			return new ResponseEntity<>(HttpStatus.PERMANENT_REDIRECT);
+//		}
+//		return null;
+//	}
 	
 	@PostMapping("/login")
 	public ResponseEntity<AccountResponseDTO> loginAccount(@RequestBody AccountLoginDTO login) {
